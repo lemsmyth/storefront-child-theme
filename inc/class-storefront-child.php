8f39c360e8959455f87df861c14e503a88dae485
Lem <lemsmyth@yahoo.com>
2020-08-03 10:32:37 -0500

    Storefront_Child class and Storefront_Child_Customizer class

---
+++ inc/class-storefront-child.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Storefront Child Class
+ *
+ * @package  storefront-child
+ * @since    1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
---
+++ inc/customizer/class-storefront-child-customizer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Storefront Child Customizer Class
+ *
+ * @package  storefront-child
+ * @since    1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
